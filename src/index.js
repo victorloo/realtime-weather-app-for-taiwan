1a713512491d1ab05dc7e7bd47776830f43ca006
victorloo <ila9970@gmail.com>
2021-05-29 16:14:02 +0800

    4.2 認識專案資料夾結構與檔案下載

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
+// Step1: 載入 React 相關套件
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+// Step2: 載入 CSS 和 React 元件
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+// Step 3: 將 React 元件和 HTML 互相綁定
 ReactDOM.render(
   <React.StrictMode>
     <App />
