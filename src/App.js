e067b421cde8e6e34bb9012faa128f457fc0b111
victorloo <ila9970@gmail.com>
2021-05-29 16:59:19 +0800

    4.3 用 JavaScripts 寫 CSS!? CSS in JS 的使用-使用 emotion

--- src/App.js
+++ src/App.js
@@ -1,13 +1,31 @@
 import React from 'react';
-import './App.css';
+// Step1: 載入 emotion 的 styled 套件
+import styled from '@emotion/styled';
+// Step2: 定義帶有 styled 的 component
+const Container = styled.div`
+  background-color: #ededed;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const WeatherCard = styled.div`
+  position: relative;
+  min-width: 360px;
+  box-shadow: 0 1px 3px 0 #999999;
+  background-color: #f9f9f9;
+  box-sizing: border-box;
+  padding: 30px 15px;
+`;
 
 function App() {
   return (
-    <div className="container">
-      <div className="weather-card">
+    <Container>
+      <WeatherCard>
         <h1>Weather</h1>
-      </div>
-    </div>
+      </WeatherCard>
+    </Container>
   );
 }
 
